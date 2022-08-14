12cc38f912a704cdcf8ff096d1d0583befe084a7
MartaMalovana <marta.malovana@gmail.com>
2022-08-14 17:02:34 +0300

    add accordion

---
+++ js/accordion.js
@@ -0,0 +1,19 @@
+function Accordion (number) {
+    const block = document.querySelector(`.${number}_title`);
+    const hiddenBlock = document.querySelector(`.${number}_article`);
+    const arrow = document.querySelector(`#${number}_arrow`);
+
+    if(hiddenBlock.style.display === "flex") {
+        hiddenBlock.style.display = "none";
+        block.style.backgroundColor = "white";
+        block.style.color = "inherit";
+        arrow.classList.replace("fa-angle-up", "fa-angle-down");
+    } else {
+        hiddenBlock.style.display = "flex";
+        block.style.backgroundColor = "#fdb526";
+        block.style.color = "white";
+        arrow.classList.replace("fa-angle-down", "fa-angle-up");
+    };
+
+    return block;
+}
